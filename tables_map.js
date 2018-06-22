0a4be82975e7889acb0e6552ed810b79ea65e327
SofiyaHevorhyan <avisventus@gmail.com>
2018-06-22 16:34:22 +0300

    Changes in design

--- tables_map.js
+++ tables_map.js
@@ -1,33 +1,27 @@
-// File: table_map.js
+// File: tables_map.js
 // Module representing of seats and chairs on the map
 
 const PRICE_1 = 100, PRICE_2 = 50, PRICE_3 = 10, PRICE_4 = 5;
 const NUM_SEATS_1 = 6, NUM_SEATS_2 = 7;
 const tables_info = {
-    "high_class": [PRICE_1, "#FAFF37"],
-    "middle_class": [PRICE_2, "#FF7097"],
-    "low_class": [PRICE_3, "#7671FF"],
-    "test_class": [PRICE_4, "#26FF49"]
+    "high_class": [PRICE_1, "#95b5f2"],
+    "middle_class": [PRICE_2, "#db3d77"],
+    "low_class": [PRICE_3, "#9edf02"]
 };
 
 class Furniture {
-    constructor(x, y, color, id, radius, status) {
+    constructor(x, y, color, id, radius) {
         this.x = x;
         this.y = y;
         this.color = color;
         this.id = id;
         this.radius = radius;
-        this._status = status;
-    }
-
-    get status() {
-        return this._status
     }
 }
 
 class Table extends Furniture {
-    constructor(x, y, id, class_type, color, radius = 80, status = "unavailable") {
-        super(x, y, color, id, radius, status);
+    constructor(x, y, id, class_type, color, radius = 80) {
+        super(x, y, color, id, radius);
         this.class_type = class_type;
     }
 
@@ -47,17 +41,16 @@ class Table extends Furniture {
         let table = paper.circle(this.x * this.radius,
             this.y * this.radius, this.radius).attr({stroke: "none", fill: this.color, opacity: .4});
 
-        // radius of first seat is counted as Table.x + 2*Table.radius
+        // define coefficient
         let x = 1.2 * this.x;
 
         let y = this.y;
-        // identify coordinates for 1 seat and cos for shift
 
         for (let i = 0; i < seats_number; i++) {
             const seat = new Seat(x, y, i + 1, tables_info[this.class_type][0]);
             const radius_coef = this.radius / seat.radius;
             seat.raphael = paper.circle(seat.x*radius_coef*seat.radius,
-                seat.y*radius_coef*seat.radius, seat.radius).attr({stroke: "none", fill: this.color, opacity: .4});
+                seat.y*radius_coef*seat.radius, seat.radius).attr({stroke: "none", fill: seat.color, opacity: .4});
             let points = this.find_point(x, y, alpha);
             x = points[0];
             y = points[1];
@@ -66,24 +59,16 @@ class Table extends Furniture {
 }
 
 class Seat extends Furniture {
-    constructor(x, y, id, price, color = "#FF0000", radius = 30, status = "available") {
+    constructor(x, y, id, price, color = "#6a0146", radius = 30) {
         super(x, y, color, id, radius, status);
         this.price = price;
         this.raphael = null;
-    }
-
-    mouseover() {
-        this.raphael.attr("opacity", 1);
-    }
-
-    mouseout() {
-        this.raphael.attr("opacity", .4);
     }
 }
 
 window.onload = function () {
     let tables = [{"x": 10, "y": 10, "class_type": "high_class"},
-        {"x": 10, "y": 20, "class_type": "test_class"},
+        {"x": 10, "y": 20, "class_type": "high_class"},
         {"x": 10, "y": 30, "class_type": "middle_class"},
         {"x": 10, "y": 40, "class_type": "low_class"}];  //coordinates of tables
 
